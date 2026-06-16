ac9047b280d91fa47ff35331873d7bf20b9d5a78
Vijaysinhh <vijaysinhjadhav23@gmail.com>
2026-06-16 08:39:27 +0530

    Add TestNG listener for test execution events

--- src/test/java/tests/AmazonTest.java
+++ src/test/java/tests/AmazonTest.java
@@ -1,11 +1,12 @@
 package tests;
-
+import org.testng.annotations.Listeners;
+import listeners.TestListener;
 import base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import utility.TestDataProvider;
-
+@Listeners(TestListener.class)
 public class AmazonTest extends BaseTest {
 
     @Test(
